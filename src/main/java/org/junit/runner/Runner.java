57abcc957cec3095808cdd2bbf3f30193a02d1b1
wangcheng <wangcheng@tezign.com>
2021-05-27 18:00:05 +0800

    Runner

--- src/main/java/org/junit/runner/Runner.java
+++ src/main/java/org/junit/runner/Runner.java
@@ -19,6 +19,14 @@ import org.junit.runner.notification.RunNotifier;
  * @see org.junit.runner.RunWith
  * @since 4.0
  */
+
+/*
+Runner运行测试，并在运行时将重要事件通知RunNotifier。
+使用@RunWith调用自定义runner时，需要继承Runner。
+创建自定义runner时，除了在此处实现抽象方法外，还必须提供一个构造函数，该构造函数将包含测试的Class作为参数。
+
+默认的运行器实现保证在运行测试之前立即构造测试用例类的实例，并且运行器将不保留对测试用例实例的引用，通常使它们可用于垃圾回收。
+ */
 public abstract class Runner implements Describable {
     /*
      * (non-Javadoc)
